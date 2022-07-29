c700091c3349d121b534ac06ea1321bc8f71255c
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-29 02:37:46 +0200

    Added show other image processing methods like cropping, showing, rotating and resizing

--- image.py
+++ image.py
@@ -1,8 +1,15 @@
 from PIL import Image, ImageFilter
+
 img = Image.open('./Pokedex/004 pikachu.jpg')
 filtered_img = img.filter(ImageFilter.BLUR)
 # filtered_img = img.convert('L')
 filtered_img.save('grey.png', 'png')
+# filtered_img.rotate(90)
+# filtered_img.resize((300, 300))
+# filtered_img.show()
+# # filtering
+# box = (100, 100, 400, 400)
+# region = filtered_img.crop(box)
 print(img.format)
 print(img.size)
 print(img.mode)
