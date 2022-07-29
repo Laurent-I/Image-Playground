bc48b6b0ae44761171f5200819a87b1bee4ef2bf
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-29 02:27:54 +0200

    filtering the image with the PIL module to blur and other cool things

--- image.py
+++ image.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageFilter
 img = Image.open('./Pokedex/004 pikachu.jpg')
-# filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img = img.convert('L')
+filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img = img.convert('L')
 filtered_img.save('grey.png', 'png')
 print(img.format)
 print(img.size)
